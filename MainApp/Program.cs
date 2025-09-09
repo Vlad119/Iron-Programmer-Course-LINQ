f579b12c4d89857f6e990b9f5a0fdfd339fe6fd2
Vlad119 <unstarkble12@gmail.com>
2025-09-09 12:57:24 +0300

    Решил задачу 7-1-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,18 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var names = new List<string> { "Ио", "Марк", "Роксана", "Светлана" };
+        foreach (var name in GetLongNames(names))
+            Console.WriteLine(name);
+    }
+
+    static List<string> GetLongNames(List<string> names)
+    {
+        return names.Where(name => name.Length > 3).ToList();
     }
 }
