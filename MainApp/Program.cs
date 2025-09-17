87c21661e830ce8197f0ae4be8b54bbdf277b25b
Vlad119 <unstarkble12@gmail.com>
2025-09-17 19:42:45 +0300

    Решил задачу 8-1-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,26 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var num = 2;
+        var colorSequence = GetColorOfLine().GetEnumerator();
+        for (int i = 0; i < num; i++)
+        {
+            if (colorSequence.MoveNext())
+                Console.WriteLine(colorSequence.Current);
+        }
+    }
+
+    static IEnumerable<string> GetColorOfLine()
+    {
+        while (true)
+        {
+            yield return "Черное";
+            yield return "Белое";
+        }
     }
 }
+
