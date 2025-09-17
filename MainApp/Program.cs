1752218d77ff34150e999dfceb569664b2f43ae6
Vlad119 <unstarkble12@gmail.com>
2025-09-17 21:32:21 +0300

    Решил задачу 8-1-7

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var number = 1;
+        var divisionNumbers = Division(number);
+        foreach (var divisionNumber in divisionNumbers)
+        {
+            Console.WriteLine(divisionNumber);
+        }
+    }
+
+    private static IEnumerable<int> Division(int n)
+    {
+        int i = 1;
+        if (n == 0)
+        {
+            throw new Exception("Поймали: Attempted to divide by zero.");
+        }
+        while (i <= 10)
+        {
+            yield return i++/n;
+        }
     }
 }
