f275400b7b5ab0010590ef5ed446fda1d4698d4b
Vlad119 <unstarkble12@gmail.com>
2025-09-17 20:25:48 +0300

    Решил задачу 8-1-6

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,33 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
-    static void Main(string[] args)
+
+    static int n = 5;
+    static IEnumerable<int> Numbers
     {
-        
+        get
+        {
+            int def = n;
+            while (n != 0)
+            {
+                yield return n--;
+            }
+            while (n <= def)
+            {
+                yield return n++;
+            }
+        }
+    }
+
+
+    static void Main()
+    {
+
+        foreach (var number in Numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
