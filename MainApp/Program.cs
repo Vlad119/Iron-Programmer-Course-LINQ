defac456ea04386f56f8044fbf02558a4ce22c74
Vlad119 <unstarkble12@gmail.com>
2025-09-17 20:08:24 +0300

    Решил задачу 8-1-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = GetInput();
+        var count = 0;
+        foreach (var line in input)
+        {
+            count++;
+        }
+        Console.WriteLine(count);
+    }
+
+    static IEnumerable<string> GetInput()
+    {
+        string line = string.Empty;
+        while ((line = Console.ReadLine()) != "Стоп")
+        {
+            yield return line;
+        }
     }
 }
