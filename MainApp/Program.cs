a638b892776df9900b912690701182c23fe828e0
Vlad119 <unstarkble12@gmail.com>
2025-09-18 20:43:14 +0300

    Решил задачу 9-2-10

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,46 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var users = new List<User>
+            {
+                new User("Алина", 108, true),
+                new User("Света", 30, false),
+                new User("Карина", 30, true)
+            };
+
+        var result = GetUser(users);
+        foreach (User user in result)
+        {
+            Console.WriteLine(user.Name);
+        }
+
+        static IEnumerable<User> GetUser(IEnumerable<User> users)
+        {
+            return users.Where(u => u.SchoolNumber == 30 || u.SchoolNumber == 108).Where(u=>u.LikeOrMessages);
+        }
     }
+}
+class User
+{
+    public User(string name, int schoolNumber, bool likeOrMessages)
+    {
+        Name = name;
+        SchoolNumber = schoolNumber;
+        LikeOrMessages = likeOrMessages;
+    }
+
+    //Имя пользователя
+    public string Name { get; set; }
+
+    //Номер школы
+    public int SchoolNumber { get; set; }
+
+    //Флаг указывающий отпралял ли пользователь сообщения или лайки
+    public bool LikeOrMessages { get; set; }
+
 }
