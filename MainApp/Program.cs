f2d82fd96dd16e58258b9db56f25ba347a5417cd
Vlad119 <unstarkble12@gmail.com>
2025-09-18 20:10:26 +0300

    Решил задачу 9-2-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,32 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var user1 = new User() { Name = "Петя", Age = 15 };
+        var user2 = new User() { Name = "Вася", Age = 18 };
+        var user3 = new User() { Name = "Оля", Age = 20 };
+        var users = new List<User>();
+        users.Add(user1);
+        users.Add(user2);
+        users.Add(user3);
+        var users18 = GetAdulthood(users);
+        foreach (var user in users18)
+        {
+            Console.WriteLine($"Пользователь {user.Name}, возраст {user.Age}");
+        }
     }
+
+    static IEnumerable<User> GetAdulthood(IEnumerable<User> users)
+    {
+        return users.Where(user => user.Age >= 18);
+    }
+}
+class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
 }
