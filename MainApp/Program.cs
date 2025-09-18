e96cc4431333d0419d6920a62cd2ad36bee886b5
Vlad119 <unstarkble12@gmail.com>
2025-09-18 20:13:07 +0300

    Решил задачу 9-2-5

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -9,10 +9,12 @@ internal class Program
         var user1 = new User() { Name = "Петя", Age = 15 };
         var user2 = new User() { Name = "Вася", Age = 18 };
         var user3 = new User() { Name = "Оля", Age = 20 };
+        var user4 = new User() { Name = "Маша", Age = 20 };
         var users = new List<User>();
         users.Add(user1);
         users.Add(user2);
         users.Add(user3);
+        users.Add(user4);
         var users18 = GetAdulthood(users);
         foreach (var user in users18)
         {
@@ -22,7 +24,7 @@ internal class Program
 
     static IEnumerable<User> GetAdulthood(IEnumerable<User> users)
     {
-        return users.Where(user => user.Age >= 18);
+        return users.Where(user => user.Age >= 18).Where(user=> user.Name.StartsWith("Ма"));
     }
 }
 class User
