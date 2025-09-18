5f3e3572c04f4db6700a4296a465377496e91278
Vlad119 <unstarkble12@gmail.com>
2025-09-18 20:18:34 +0300

    Решил задачу 9-2-6

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        string input = Console.ReadLine();
+        var numbers = new List<int>();
+
+        foreach (var number in input.Split())
+        {
+            numbers.Add(Convert.ToInt32(number));
+        }
+
+        var result = numbers.Where(n => n % 2 == 0);
+        Console.WriteLine(string.Join(" ", result));
     }
 }
