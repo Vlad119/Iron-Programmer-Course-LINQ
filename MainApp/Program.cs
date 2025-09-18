6ee2278847900e7c8694dd69d216bc0220af4030
Vlad119 <unstarkble12@gmail.com>
2025-09-18 19:48:55 +0300

    Решил задачу 9-2-1

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+static class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var numbersBiggerThanFive = MyWhere(numbers, x => x > 5);
+        Console.WriteLine(string.Join(" ", numbersBiggerThanFive));
+    }
+
+    static IEnumerable<T> MyWhere<T>(this IEnumerable<T> numbers, Func<T, bool> rule)
+    {
+        foreach (var item in numbers)
+        {
+            if (rule(item))
+            {
+                yield return item;
+            }
+        }
     }
 }
