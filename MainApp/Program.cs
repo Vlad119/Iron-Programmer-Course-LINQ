b567d0d1706ecf460113a7dbfae04e0a211f68a0
Vlad119 <unstarkble12@gmail.com>
2025-09-22 16:19:02 +0300

    Решил задачу 9-3-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,35 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = "1 2 3 4";
+        var result = GetNumbers(numbers);
+        Console.WriteLine(string.Join(' ', result));
+    }
+
+    //static int[] GetNumbers(string input)
+    //{
+    //    Делим строку по пробелам и получим массив строк
+    //    var data = input.Split();
+    //    Заведем НОВЫЙ массив чисел
+    //    var numbers = new int[data.Length];
+    //    Проходим по массиву строк
+    //    for (int i = 0; i < data.Length; i++)
+    //    {
+    //        Конвертируем каждый строковый элемент в число
+    //        var number = Convert.ToInt32(data[i]);
+
+    //        Записываем в массив чисел
+    //        numbers[i] = number;
+    //    }
+    //    return numbers;
+    //}
+
+    static int[] GetNumbers(string input)
+    {
+        return input.Split().Select(x=>Convert.ToInt32(x)).ToArray();
     }
 }
