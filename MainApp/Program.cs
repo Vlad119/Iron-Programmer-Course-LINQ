f93a6936755ac099f9efb7511db993bae74cba47
Vlad119 <unstarkble12@gmail.com>
2025-09-22 16:24:44 +0300

    Решил задачу 9-3-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,19 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int n = 5;
+        var result = Method(numbers, n);
+        Console.WriteLine(string.Join(" ", result));
+    }
+
+    static IEnumerable<int> Method(IEnumerable<int> numbers, int n)
+    {
+        return numbers.Where(x => x > n).Select(x => x * x);
     }
 }
