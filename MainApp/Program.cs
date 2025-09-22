64c5314b242b5aeb9e721299030e101e2b3e2f5a
Vlad119 <unstarkble12@gmail.com>
2025-09-22 17:59:47 +0300

    Решил задачу 9-3-6

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var split = Console.ReadLine().Split(' ');
+        Console.WriteLine(string.Join(" ", split.Select(int.Parse).Select(s => ++s)));
     }
 }
