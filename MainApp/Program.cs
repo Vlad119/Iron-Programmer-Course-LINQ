e65afb23e209de81a2a7b7a9d4bcce5709625908
Vlad119 <unstarkble12@gmail.com>
2025-09-22 16:52:31 +0300

    Решил задачу 9-3-5

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var users = new List<User>()
+            {
+                new User(){Name="Иосиф", Age=26},
+                new User(){Name="Алексей", Age=17},
+                new User(){Name="Николай", Age=19},
+                new User(){Name="Наталья", Age=20},
+                new User(){Name="Роксана", Age=16}
+            };
+        var names = GetAdulthoodNames(users);
+        foreach (var name in names)
+        {
+            Console.WriteLine($"{name} - совершеннолетный гражданин");
+        }
     }
+
+    private static IEnumerable<string> GetAdulthoodNames(IEnumerable<User> users)
+    {
+        return users.Where(u => u.Age >= 18).Select(u => u.Name);
+    }
+}
+
+class User
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
 }
