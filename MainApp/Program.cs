a53dea45b1e9f6be87b93724b0752eece67705ec
Vlad119 <unstarkble12@gmail.com>
2025-09-23 11:33:08 +0300

    Решил задачу 9-3-9

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,20 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        string protocol = Console.ReadLine();
+        string domain = Console.ReadLine();
+        int limit = int.Parse(Console.ReadLine());
+        var paths = new List<string>();
+        for (int i = 0; i < limit; i++)
+        {
+            paths.Add(Console.ReadLine());
+        }
+        var absolutePaths = paths.Select(path => $"{protocol}://{domain}/{path}");
+        Console.WriteLine(string.Join("\n", absolutePaths));
     }
 }
