b951438ff1229e0c5377d9a81dd39526efd6b625
Vlad119 <unstarkble12@gmail.com>
2025-09-23 10:27:15 +0300

    Решил задачу 9-3-7

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,54 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var productDb1 = new ProductDb()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Молоко",
+            Cost = 120,
+            Description = "Натуральное молоко из деревни",
+            ImagePath = "https://lenta.gcdn.co/globalassets/1/-/10/20/65/346818_2.png?preset=fulllossywhite"
+        };
+        var productDb2 = new ProductDb()
+        {
+            Id = Guid.NewGuid(),
+            Name = "Сок",
+            Cost = 50,
+            Description = "Среднегазированный",
+            ImagePath = "test/image"
+        };
+        var productsDb = new List<ProductDb>
+    {
+        productDb1, productDb2
+    };
+        var productViewModels = productsDb.ToProductViewModels();
+        foreach (var item in productViewModels)
+        {
+            Console.WriteLine(item.Name);
+            Console.WriteLine(item.Cost);
+        }
     }
+
+   
 }
+
+public class ProductDb
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Cost { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+}
+
+public class ProductViewModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public decimal Cost { get; set; }
+}
+
---
+++ MainApp/ViewModelsExtenssion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class ProductExtensions
+{
+    public static IEnumerable<ProductViewModel> ToProductViewModels(this IEnumerable<ProductDb> source)
+    {
+        return source.Select(db => new ProductViewModel
+        {
+            Id = db.Id,
+            Name = db.Name,
+            Cost = db.Cost
+        });
+    }
+}
