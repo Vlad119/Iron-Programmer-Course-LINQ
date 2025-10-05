f866155b34f14f40f0b9afc4d4a1a34aac3a53d2
Vlad119 <unstarkble12@gmail.com>
2025-10-05 11:14:29 +0300

    Решил задачу 9-5-1

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = Console.ReadLine().Split(',').Select(int.Parse);
+        var result = numbers.SelectMany((x, index) => new[] { $"{x - 1} {index}", $"{x} {index}", $"{x + 1} {index}" });
+        Console.WriteLine(string.Join("\n", result));
     }
 }
