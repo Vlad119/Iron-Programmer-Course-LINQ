ced97a7983808a103c63d8ebe8f9a8f0d0f51f3f
Vlad119 <unstarkble12@gmail.com>
2025-10-12 14:45:00 +0300

    Решил задачу 9-5-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,14 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var first = Console.ReadLine().Split(',').Select(int.Parse);
+        var second = Console.ReadLine().Split(',').Select(int.Parse);
+        var result = first.SelectMany(first => second,
+        (first, second) => $"{first}+{second}={first + second}");
+        Console.WriteLine(string.Join("\n", result));
     }
 }
