b772dbfa05ea50f2d26d2aa6e2514d6fea9a8356
Vlad119 <unstarkble12@gmail.com>
2025-10-13 23:49:14 +0300

    Решил задачу 9-5-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split(',').Select(int.Parse);
+        var result = input.SelectMany(res => input,
+        (a, b) => $"{a}x{b}={a * b}");
+        Console.WriteLine(string.Join("\n", result));
     }
 }
