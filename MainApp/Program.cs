b7da71020a4f26da32b51a2b8ee0d22ae5fdef4f
Vlad119 <unstarkble12@gmail.com>
2025-10-14 17:24:02 +0300

    Решил задачу 10-1-1

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,26 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new List<int> { 1, 2, 3, 4, 5 };
+        var result = SumExtension.MySum(numbers);
+        Console.WriteLine(result);
+    }
+}
+
+public static class SumExtension
+{
+    public static int MySum(this IEnumerable<int> items)
+    {
+        int sum = 0;
+        foreach (int item in items)
+        {
+            sum += item;
+        }
+        return sum;
     }
 }
