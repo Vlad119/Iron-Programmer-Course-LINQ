2aca2e5862451e1ff4fd6a0a7b04032a5ad88de8
Vlad119 <unstarkble12@gmail.com>
2025-10-14 18:25:38 +0300

    Решил задачу 10-1-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split().Select(int.Parse);
+        var result = input.Where(x => x % 2 ==0).Sum();
+        Console.WriteLine(result);
     }
 }
