4a0f9d21ec60777c46ce85d2bd0cf6a02db320fc
Vlad119 <unstarkble12@gmail.com>
2025-10-14 16:05:02 +0300

    Решил задачу 9-5-5

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,27 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        List<string[]> teams = new List<string[]>
+            {
+                new string[]{"Лионель Месси", "Криштиану Роналду", "Златан Ибрагимович", "Роберт Левандовский"},
+                new string[]{"Мохаммед Салах", "Паоло Мальдини", "Франц Беккенбауэр", "Мишель Платини"},
+                new string[]{"Йохан Кройф", "Лев Яшин", "Дэвид Бэкхем", "Диего Марадона"}
+            };
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        var result = teams.SelectMany((team, teamIndex) =>
+        team.Select((player, playerIndex) =>
+        $"{player} играет в команде №{teamIndex + 1} под номером {playerIndex + n}"));
+
+        foreach (var playerInfo in result)
+        {
+            Console.WriteLine(playerInfo);
+        }
     }
 }
