32e6ae95eaa4ebff03551164a0f9e2055d5373c4
Vlad119 <unstarkble12@gmail.com>
2025-10-15 19:33:26 +0300

    Решил задачу 10-1-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var phones = new Phone[]
+    {
+        new Phone("Iphone 13 mini", 70_000),
+        new Phone("Honor 50 Light", 20_000)
+
+    };
+
+        var sum = phones.MySum2();
+        Console.WriteLine(sum);
     }
 }
+
+public class Phone
+{
+    public string Name { get; set; }
+    public decimal Cost { get; set; }
+    public Phone(string name, decimal cost)
+    {
+        Name = name;
+        Cost = cost;
+    }
+}
+
---
+++ MainApp/SumExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+public static class SumExtension
+{
+    public static decimal MySum2(this IEnumerable<Phone> phones)
+    {
+        decimal sum = 0;
+        foreach (var phone in phones)
+        {
+            sum += phone.Cost;
+        }
+        return sum;
+    }
+}
