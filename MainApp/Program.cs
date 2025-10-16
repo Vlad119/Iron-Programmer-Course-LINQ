bed857a61cfddaaedf54993b540c0a7003d77394
Vlad119 <unstarkble12@gmail.com>
2025-10-16 13:37:45 +0300

    Решил задачу 10-1-5

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+public static class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        
+        var result = Console.ReadLine().Split(", ").Select(num => num.Sum(c => c - '0'));
+        Console.WriteLine(string.Join(", ", result));
     }
 }
