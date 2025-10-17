a179639da68f9a5a840ed63e4ae0b8c3d40d92b7
Vlad119 <unstarkble12@gmail.com>
2025-10-17 11:45:20 +0300

    Решил задачу 10-2-1

---
+++ MainApp/MinExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class MinExtension
+{
+    public static int MyMin(this IEnumerable<int> numbersSource)
+    {
+        using (var numbers = numbersSource.GetEnumerator()) // using для освобождения ресурсов
+        {
+            if (!numbers.MoveNext()) // Проверяем, есть ли элементы
+            {
+                throw new InvalidOperationException("Коллекция пуста");
+            }
+            int min = numbers.Current;
+            int current;
+            while (numbers.MoveNext())
+            {
+                current = numbers.Current;
+                if (current < min) min = current;
+            }
+            return min;
+        }
+    }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new List<int> { 1, 2, 3, 4, 5 };
+        var result = MinExtension.MyMin(numbers);
+        Console.WriteLine(result);
     }
 }
