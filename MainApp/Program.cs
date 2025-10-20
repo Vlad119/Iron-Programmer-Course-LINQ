01e6c4d362cc609c9ad40a35711b9af25ed02a76
Vlad119 <unstarkble12@gmail.com>
2025-10-20 18:24:37 +0300

    Решил задачу10-2-6

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,39 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        //var num = int.Parse(Console.ReadLine());
+        var users = new List<User>
+    {
+        new User ("Анна", "Scorpion", 22),
+        new User ("Николай", "TheDestroyer817", 28),
+        new User ("Сергей", "RainingMan", 35),
+        new User ("Андрей", "Mr.A", 19)
+    };
+
+        var result = GetShortestNicknameLength(users);
+        Console.WriteLine(result);
+    }
+
+    static int GetShortestNicknameLength(IEnumerable<User> users)
+    {
+        return users.Min(x => x.Nickname.Length);
+    }
+}
+public class User
+{
+    public string Name { get; set; }
+    public string Nickname { get; set; }
+    public int Age { get; set; }
+
+    public User(string name, string nickname, int age)
+    {
+        Name = name;
+        Nickname = nickname;
+        Age = age;
     }
 }
