deacf987436f03cc6b650fae74ef1d8b39ea9395
Vlad119 <unstarkble12@gmail.com>
2025-10-21 12:55:29 +0300

    Решил задачу 10-2-7

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,22 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        
+        string input = Console.ReadLine();
+        int splitIndex = input.IndexOf(':');
+        string gradesPart = input.Substring(splitIndex + 1).Trim();
+        var grades = gradesPart.Split(',').Select(int.Parse);
+        double average = grades.Average();
+        int finalGrade = 2;
+        if (average > 4.5 && average <= 5.0)
+            finalGrade = 5;
+        else if (average > 3.5 && average <= 4.5)
+            finalGrade = 4;
+        else if (average > 2.5 && average <= 3.5)
+            finalGrade = 3;
+        Console.WriteLine(Math.Round(average));
     }
 }
