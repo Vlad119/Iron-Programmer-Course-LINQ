5c519d3f6fce0f9e66dd8c636cf458072ca2ed90
Vlad119 <unstarkble12@gmail.com>
2025-10-21 12:37:33 +0300

    Решил задачу 10-2-7

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var result = Console.ReadLine().Split().Select(num => num.Sum(c => c - '0')).Max();
+        Console.WriteLine(result);
     }
 }
