02174b19e9839c7263d6cee12ca1864ae5499169
Vlad119 <unstarkble12@gmail.com>
2025-10-21 14:00:59 +0300

    Решил задачу 11-1-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        string input = Console.ReadLine();
+        Console.WriteLine(input.Count(char.IsLetter));
     }
 }
