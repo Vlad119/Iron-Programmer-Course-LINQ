ada4376f7b647ebb30e5bfffaf9da7fe1c5fa45f
Vlad119 <unstarkble12@gmail.com>
2025-10-21 14:13:53 +0300

    Решил задачу 11-1-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,44 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        
+        var students = GetStudentsFromInput();
+        var age = Convert.ToInt32(Console.ReadLine());
+        var result = GetСount(students, age);
+        Console.WriteLine(result);
+    }
+
+    static int GetСount(IEnumerable<Student> students, int age)
+    {
+        return students.Count(x => x.Age == age);
+    }
+
+    static IEnumerable<Student> GetStudentsFromInput()
+    {
+        return new List<Student>
+    {
+       new Student ("Joseph", 22 ),
+       new Student ("Oleg", 23 ),
+       new Student ("Petr", 22 ),
+       new Student ("Anna", 24 ),
+       new Student ("Svetlana", 19 ),
+       new Student ("Ekatherina", 18 ),
+       new Student ("Ekatherina", 22 )
+    };
+    }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
     }
 }
