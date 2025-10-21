bcabdd604b4b44e73bf13f4ebd7d19cd2e1db50d
Vlad119 <unstarkble12@gmail.com>
2025-10-21 13:39:46 +0300

    Решил задачу 11-1-1

---
+++ MainApp/CountExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+public static class CountExtension
+{
+    public static int MyCount(this IEnumerable<int> items)
+    {
+        int count = 0;
+        foreach (var item in items) count++;
+        return count;
+    }
+}
+
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var strings = "2";
+        var numbers = new int[strings.Length];
+        for (int i = 0; i < strings.Length; i++)
+        {
+            numbers[i] = int.Parse(strings[i].ToString());
+        }
+        var first = CountExtension.MyCount(numbers);
+        Console.WriteLine(first);
     }
 }
