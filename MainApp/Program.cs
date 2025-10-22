0d40cfaa7da3a8470bcbba4e8dda786298f43ca8
Vlad119 <unstarkble12@gmail.com>
2025-10-22 18:26:40 +0300

    Решил задачу 11-1-5

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine();
+        var result = input.Count(c => c == '+') - input.Count(c => c == '-');
+        Console.WriteLine(Math.Abs(result));
     }
 }
