fcd2761ddbbc69e82400bbc60e7f2459fe9a3244
Vlad119 <unstarkble12@gmail.com>
2025-10-25 14:48:13 +0300

    Решил задачу 11-4-1

---
+++ MainApp/DictionaryExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class DictionaryExtension
+{
+    public static Dictionary<int, Employee> MyToDictionary(this IEnumerable<Employee> employees)
+    {
+        var result = new Dictionary<int, Employee>();
+        foreach (var employee in employees)
+        {
+            if (result.ContainsKey(employee.Id))
+            throw new Exception($"Ключ {employee.Id} уже существует.");
+            result.Add(employee.Id, employee);
+        }
+        return result;
+    }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,39 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var employees = GetEmployeesFromInput();
+        var dictionary = DictionaryExtension.MyToDictionary(employees);
+        foreach (var keyValuePair in dictionary)
+        {
+            Console.WriteLine($"{keyValuePair.Key} - {keyValuePair.Value.Name}");
+        }
+    }
+
+    static IEnumerable<Employee> GetEmployeesFromInput()
+    {
+        int n = 6;
+        return new List<Employee>
+     {
+         new Employee (1, "Василий"),
+         new Employee (2, "Иван"),
+         new Employee (3, "Петр"),
+         new Employee (4, "Семен"),
+         new Employee (5, "Валерий"),
+         new Employee (6, "Николай")
+   };
+    }
+}
+public class Employee
+{
+    public int Id { get; }
+    public string Name { get; }
+    public Employee(int id, string name)
+    {
+        Id = id;
+        Name = name;
     }
 }
