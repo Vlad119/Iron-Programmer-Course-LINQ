b3b0fbf779c6b576535b03271c833a76af4d62bc
Vlad119 <unstarkble12@gmail.com>
2025-10-25 14:33:46 +0300

    Решил задачу 11-3-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        
+        string mask = Console.ReadLine();
+        string input = Console.ReadLine();
+        char[] separators = mask.Where(c => !char.IsDigit(c)).ToArray();
+        string[] parts = input.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        string result = string.Join(" ", parts);
+        Console.WriteLine(result);
     }
 }
