cf3b6e2dc5304a1cec661c22926c4b8b29e40668
Vlad119 <unstarkble12@gmail.com>
2025-10-25 14:20:38 +0300

    Решил задачу 11-3-1

---
+++ MainApp/ArrayExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public static class ArrayExtension
+{
+    public static T[] MyToArray<T>(this IEnumerable<T> items)
+    {
+        var size = items.Count();
+        var result = new T[size];
+        var index = 0;
+        foreach (var item in items)
+        {
+            result[index] = item;
+            index++;
+        }
+        return result;
+    }
+}
+
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new int[] { 1, 11, 2, 22, 3 };
+        var result = ArrayExtension.MyToArray(numbers);
+        Console.WriteLine(string.Join(" ", result));
     }
 }
