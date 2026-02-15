1dd98a5602e40b10ce211f9fef8c0da722cae5aa
Vlad119 <unstarkble12@gmail.com>
2026-02-15 15:53:36 +0300

    Решил задачу 11-5-1

---
+++ MainApp/MyToHashSetExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+internal static class MyToHashSetExtension
+{
+    public static HashSet<T> MyToHashSet<T>(this IEnumerable<T> items)
+    {
+        var result = new HashSet<T>();
+        foreach (var item in items)
+        {
+            result.Add(item);
+        }
+        return result;
+    }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new int[] { 1, 11, 2, 22, 3 };
+        var result = numbers.MyToHashSet();
+        Console.WriteLine(string.Join(" ", result));
     }
 }
