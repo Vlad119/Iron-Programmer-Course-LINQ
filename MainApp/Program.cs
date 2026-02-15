b5f92ee0398a37d0568679bb958edabc9379d474
Vlad119 <unstarkble12@gmail.com>
2026-02-15 15:30:04 +0300

    Решил задачу 11-4-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split(", ");
+        var dict = input.ToDictionary(x => x, x => int.Parse(x) % 2 != 0);
+        foreach (var element in dict)
+        {
+            Console.WriteLine($"{element.Key} {element.Value}");
+        }
     }
 }
