2adedb4ddcf6917a6012e06df26ff66181989dcf
Vlad119 <unstarkble12@gmail.com>
2026-02-15 15:15:53 +0300

    решил задачу 11-4-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,15 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        string input = Console.ReadLine();
+        var dictionary = input.Split(' ').ToDictionary(word => word[0], word => word);
+        foreach (var element in dictionary)
+        {
+            Console.WriteLine($"{element.Key} {element.Value}");
+        }
     }
 }
