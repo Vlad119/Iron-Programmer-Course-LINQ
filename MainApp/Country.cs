c7bf896f58da00f45ef368dbfcc5028ce6750676
Vlad119 <unstarkble12@gmail.com>
2026-02-24 15:16:53 +0300

    Решил задачу 12-1-2

---
+++ MainApp/Country.cs
@@ -0,0 +1,12 @@
+﻿internal class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Capital { get; set; }
+    public int Population { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Name} {Population}";
+    }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var country1 = new Country() { Id = 1, Name = "Russia", Capital = "Moskau", Population = 151800000 };
+        var country2 = new Country() { Id = 2, Name = "Deutschland", Capital = "Berlin", Population = 84432670 };
+        var country3 = new Country() { Id = 3, Name = "Croatia", Capital = "Zagreb", Population = 4188853 };
+        var country4 = new Country() { Id = 4, Name = "Liechtenstein", Capital = "Vaduz", Population = 38900 };
+        var country5 = new Country() { Id = 5, Name = "Vatican City State", Capital = "Vatican", Population = 825 };
+        var firstMicrostate = TryGetDwarfCountry(new List<Country> { country1, country2, country3, country4, country5 });
+        Console.WriteLine($"Первая карликовая страна: {firstMicrostate.Id} {firstMicrostate.Name} с населением {firstMicrostate.Population}");
+        //Console.WriteLine(TryGetDwarfCountry(new List<Country> { }));
+    }
+
+    static Country TryGetDwarfCountry(IEnumerable<Country> countries)
+    {
+        return countries.Where(x => x.Population >= 100_000).FirstOrDefault();
     }
 }
