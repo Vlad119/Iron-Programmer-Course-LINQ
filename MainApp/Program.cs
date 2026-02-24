f5976c018cdc980f43174a92d12c04f54b8843e3
Vlad119 <unstarkble12@gmail.com>
2026-02-24 16:28:37 +0300

    Решил задачу 12-1-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,34 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var students = new List<Student>
+    {
+        new Student ( "Svetlana", 21 ),
+        new Student ( "Joseph", 22 ),
+        new Student ( "Oleg", 23 ),
+        new Student ( "Petr", 22 ),
+        new Student ( "Anna", 24 ),
+        new Student ( "Svetlana", 19 ),
+        new Student ( "Ekatherina", 18 ),
+        new Student ( "Ekatherina", 22 )
+    };
+        var ageForFind = 20;
+        var result = GetFirst(students, ageForFind);
+        Console.WriteLine(result);
+    }
+
+    static Student GetFirst(IEnumerable<Student> students, int age)
+    {
+        var student = students.FirstOrDefault(x => x.Age == age);
+        if (student == null)
+        {
+            throw new InvalidOperationException("Студент не найден");
+        }
+        return student;
     }
 }
---
+++ MainApp/Student.cs
@@ -0,0 +1,16 @@
+﻿public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{Age}, {Name}";
+    }
+}
