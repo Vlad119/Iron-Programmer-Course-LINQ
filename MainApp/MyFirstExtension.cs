189840e2f3244fa14c0a7fcec3f46c5d1c92a62d
Vlad119 <unstarkble12@gmail.com>
2026-02-24 14:57:05 +0300

    Решил задачу 12-1-1

---
+++ MainApp/MyFirstExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal static class MyFirstExtension
+{
+    public static int MyFirst(this IEnumerable<int> items)
+    {
+        var enumerator = items.GetEnumerator();
+        if (enumerator.MoveNext())
+        {
+            return enumerator.Current;
+        }
+        throw new InvalidOperationException("Последовательность не содержит элементов");
+    }
+}
+
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new int[10];
+        var first = numbers.MyFirst();
+        Console.WriteLine(first);
     }
 }
