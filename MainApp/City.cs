8c5f2607ecd6de3ab4660cc344a13f6d348a7cdd
Vlad119 <unstarkble12@gmail.com>
2026-02-25 21:06:45 +0300

    Решил задачу 12-1-5

---
+++ MainApp/City.cs
@@ -0,0 +1,16 @@
+﻿public class City
+{
+    public string Name { get; set; }
+    public int CitizensCount { get; set; }
+
+    public City(string name, int citizensCount)
+    {
+        CitizensCount = citizensCount;
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}, {CitizensCount}";
+    }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var n = Convert.ToInt32(Console.ReadLine());
+        var cities = new List<City>(n);
+        for (int i = 0; i < n; i++)
+        {
+            var data = Console.ReadLine().Split(", ");
+            var city = new City(data[0], int.Parse(data[1]));
+            cities.Add(city);
+        }
+        var count = Convert.ToInt32(Console.ReadLine());
+        var result = cities.FirstOrDefault(x => x.CitizensCount > count);
+        Console.WriteLine(result == null ? "Нет" : result);
     }
 }
