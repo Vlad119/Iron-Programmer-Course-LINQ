33bc0ad5551aedb623d31f0cfa7593f58bdafbeb
Vlad119 <unstarkble12@gmail.com>
2026-02-25 20:55:16 +0300

    Решил задачу 12-1-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split(", ");
+        var result = input.FirstOrDefault(x => int.Parse(x) % 2 == 0);
+        Console.WriteLine(result == null ? "Подходящего числа нет" : result);
     }
 }
