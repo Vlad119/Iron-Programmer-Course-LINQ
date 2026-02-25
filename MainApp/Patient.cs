6f0e6607de97118fbadd8624a36659b544b516c5
Vlad119 <unstarkble12@gmail.com>
2026-02-25 22:26:19 +0300

    Решил задачу 12-1-7

---
+++ MainApp/Patient.cs
@@ -0,0 +1,8 @@
+﻿class Patient
+{
+    //Фамилия пациента
+    public string Surname { get; set; }
+
+    //Флаг, показывающий есть ли льгота у пациента
+    public bool Privilege { get; set; }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,25 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var patients = new List<Patient>
+    {
+        new Patient {Surname = "Петров", Privilege = false },
+        new Patient {Surname = "Иванов", Privilege = false }
+    };
+
+        var firstPatient = TryGetNextPatient(patients);
+        Console.WriteLine(firstPatient);
     }
+
+    static string TryGetNextPatient(List<Patient> patients)
+    {
+        var patient = patients.FirstOrDefault(x => x.Privilege) ?? patients.FirstOrDefault();
+        return patient?.Surname ?? "Пациентов в очереди нет";
+    }
+
 }
