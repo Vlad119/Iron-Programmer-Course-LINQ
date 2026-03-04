870ef86fe02a116d22a4deac48a8cea6b078328e
Vlad119 <unstarkble12@gmail.com>
2026-03-04 21:04:31 +0300

    Решил задачу 12-2-1

---
+++ MainApp/MyLastExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class MyLastExtension
+{
+    public static int MyLast(this IEnumerable<int> items)
+    {
+        var result = new List<int>();
+        foreach (var item in items)
+        {
+            result.Add(item);
+        }
+        return result.Count != 0 ? result[result.Count - 1] : throw new InvalidOperationException("Последовательность не содержит элементов");
+    }
+}
+
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,11 @@
-﻿internal class Program
+﻿using System;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new int[0];
+        var last = numbers.MyLast();
+        Console.WriteLine(last);
     }
 }
