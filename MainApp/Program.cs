882ad3200473b076ace7d87c745a76925c00998f
Vlad119 <unstarkble12@gmail.com>
2026-03-19 15:24:41 +0300

    Решил задачу 12-2-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split(';'); 
+        var searchString = Console.ReadLine();
+        var users = input.Select(userData => userData.Split(", "))
+        .Select(x => new User(x[0], x[1], int.Parse(x[2])));
+        var result = users.LastOrDefault(user => user.Nickname.ToLower().Contains(searchString.ToLower()));
+        Console.WriteLine(result);
     }
 }
+
---
+++ MainApp/User.cs
@@ -0,0 +1,18 @@
+﻿public class User
+{
+    public string Name { get; set; }
+    public string Nickname { get; set; }
+    public int Age { get; set; }
+
+    public User(string name, string nickname, int age)
+    {
+        Name = name;
+        Nickname = nickname;
+        Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}, {Nickname}, {Age}";
+    }
+}
