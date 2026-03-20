795a4e08b60de760e41b01a48c892eacb8ffadad
Vlad119 <unstarkble12@gmail.com>
2026-03-20 09:58:40 +0300

    Решил задачу 12-2-5

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split(", ").Select(int.Parse); ;
+        var result = input.LastOrDefault(x => x % 2 != 0);
+        Console.WriteLine(result);
     }
 }
