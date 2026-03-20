21a1d9bf1250c6fd1ace1a55431b316ec39e9086
Vlad119 <unstarkble12@gmail.com>
2026-03-20 10:17:39 +0300

    Решил задачу 12-2-6

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var firstChar = Console.ReadLine().First();
+        var secondChar = Console.ReadLine().LastOrDefault();
+        Console.WriteLine(firstChar.Equals(secondChar));
     }
 }
