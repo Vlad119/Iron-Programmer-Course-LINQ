579cd105694b325fe3a3f53c49bc75c7e9f2516e
Vlad119 <unstarkble12@gmail.com>
2026-03-24 10:16:06 +0300

    Решил задачу 12-3-1

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,39 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var students = new List<Student>
+    {
+        new Student ( "Svetlana", 21 ),
+        new Student ( "Joseph", 23 ),
+        new Student ( "Oleg", 23 ),
+        new Student ( "Petr", 23 ),
+        new Student ( "Anna", 24 ),
+        new Student ( "Svetlana", 19 ),
+        new Student ( "Ekatherina", 18 ),
+        new Student ( "Ekatherina", 22 )
+    };
+        var ageForFind = 22;
+        var result = GetSingle(students, ageForFind);
+        Console.WriteLine(result);
+    }
+
+    static Student GetSingle(IEnumerable<Student> students, int ageForFind)
+    {
+        int counter = 0;
+        Student result = null;
+        foreach (Student student in students)
+        {
+            if (student.Age == ageForFind)
+            {
+                counter++;
+                result = student;
+            }
+        }
+        if (counter == 1) return result;
+        else throw new Exception("Неверное количество студентов");
     }
 }
