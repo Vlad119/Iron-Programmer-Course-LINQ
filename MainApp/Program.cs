524ae68a09f465504fad6dcf7d899dd501b389cf
Vlad119 <unstarkble12@gmail.com>
2026-03-30 17:05:18 +0300

    Решил задачу 12-4-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        string s = Console.ReadLine();
+        for (int i = 0; i < s.Length; i++)
+        {
+            char left = s.ElementAt(i);
+            char right = s.ElementAt(s.Length - 1 - i);
+            Console.WriteLine($"{left} {right}");
+        }
     }
 }
