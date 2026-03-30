008c3e7ac9fc374e38c0cb2dcc950039f68986c8
Vlad119 <unstarkble12@gmail.com>
2026-03-30 17:19:22 +0300

    Решил задачу 12-4-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+        int index = int.Parse(Console.ReadLine());
+        int result = numbers.ElementAt(index + 1);
+        Console.WriteLine(result % 2 == 0 ? "" : result);
     }
 }
