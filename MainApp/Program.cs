f39d65e3251abfa8099d4ff9b3333f964a44665d
Vlad119 <unstarkble12@gmail.com>
2026-03-30 10:07:13 +0300

    Решил задачу 12-4-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        string word = Console.ReadLine();
+        var result = word.ElementAt((word.Length - 1) / 2);
+        Console.WriteLine(result);
     }
 }
