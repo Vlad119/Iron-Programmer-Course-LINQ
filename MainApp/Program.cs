f3cc1a3ac2937397830f4b68ad122a801f14d1f7
Vlad119 <unstarkble12@gmail.com>
2026-03-31 10:33:37 +0300

    Решил задачу 13-1-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split().Select(int.Parse).Distinct();
+        var result = input.Aggregate(1, (a, b) => a * b);
+        Console.WriteLine(result);
     }
 }
