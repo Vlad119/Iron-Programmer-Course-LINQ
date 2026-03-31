bd7dca0f6f9d8c6fa066129433700b4bca4995ad
Vlad119 <unstarkble12@gmail.com>
2026-03-31 10:00:40 +0300

    Решил задачу 13-1-1

---
+++ MainApp/MyDistinctExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public static class MyDistinctExtension
+{
+    public static IEnumerable<int> MyDistinct(this IEnumerable<int> items)
+    {
+        var result = new List<int>();
+        foreach (var item in items)
+        {
+            if(!result.Contains(item)) result.Add(item);
+        }
+        return result;
+    }
+}
--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new List<int> { 1, 2, 3, 4, 1, 2, 3 };
+        var result = numbers.MyDistinct();
+        Console.WriteLine(string.Join(" ", result));
     }
 }
