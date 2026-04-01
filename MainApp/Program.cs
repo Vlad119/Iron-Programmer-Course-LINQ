03940e0c4440720e6cb730a306684526da38883d
Vlad119 <unstarkble12@gmail.com>
2026-04-01 09:54:53 +0300

    Решил задачу 13-1-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split().Select(int.Parse);
+        var r1 = input.Where(x => x % 2 == 0).Distinct().Sum();
+        var r2 = input.Where(x => x % 2 != 0).Distinct().Sum();
+        Console.WriteLine(r1 - r2 + "");
     }
 }
