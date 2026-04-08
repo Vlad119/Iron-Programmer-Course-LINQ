ec2c7d3f07b16bb259ad91d5077b78d51a1dea6d
Vlad119 <unstarkble12@gmail.com>
2026-04-08 20:52:31 +0300

    Пример для понимания отложенного выполнения

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,29 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var numbers = new List<int> { 1, 2, 3 };
+        // Шаг 1: Объявляем запрос
+        var query = numbers.Where(n =>
+        {
+            Console.WriteLine("Проверяю число " + n);
+            return n > 1;
+        });
+
+        Console.WriteLine("Запрос создан.");
+
+        // Шаг 2: Меняем коллекцию
+        numbers.Add(4);
+        numbers.Add(5);
+
+        // Шаг 3: Начинаем перебирать
+        foreach (var q in query)
+        {
+            Console.WriteLine("Результат: " + q);
+        }
     }
 }
