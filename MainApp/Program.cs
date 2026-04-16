3ff097ca6301f928fb9be1f85c69b6386f090cf5
Vlad119 <unstarkble12@gmail.com>
2026-04-16 15:16:07 +0300

    Решил задачу 14-1-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
+
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Where(x => char.IsLetter(x)).OrderBy(x => x);
+        Console.WriteLine(string.Join("", input)); ;
     }
 }
