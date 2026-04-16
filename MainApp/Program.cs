a95a95f471dcf6a5c1a56fcd8ac9b3e327dcdf08
Vlad119 <unstarkble12@gmail.com>
2026-04-16 12:05:19 +0300

    Решил задачу 14-1-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split(' ').Select(int.Parse);
+        var result = input.Distinct().OrderBy(x => x).ToList();
+        Console.WriteLine(result[result.Count-2]);
     }
 }
