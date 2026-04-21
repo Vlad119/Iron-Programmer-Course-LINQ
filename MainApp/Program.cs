3ed77491cdd9c90896090c05c8c746cb625f4a20
Vlad119 <unstarkble12@gmail.com>
2026-04-21 16:49:50 +0300

    Решил задачу 14-1-6

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,28 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        
+        var rectagles = new List<Rectangle>
+    {
+        new Rectangle(1, 9),
+        new Rectangle(2, 8),
+        new Rectangle(3 , 7),
+        new Rectangle(4 , 6),
+        new Rectangle(5 , 5)
+    };
+        var result = SortByArea(rectagles);
+        foreach (var rectangle in result)
+        {
+            Console.WriteLine($"{rectangle.Width} {rectangle.Height}");
+        }
+    }
+
+    static IEnumerable<Rectangle> SortByArea(IEnumerable<Rectangle> rectangles)
+    {
+        return rectangles.OrderBy(x => x.Width * x.Height);
     }
 }
---
+++ MainApp/Rectangle.cs
@@ -0,0 +1,10 @@
+﻿public class Rectangle
+{
+    public int Width { get; }
+    public int Height { get; }
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+}
