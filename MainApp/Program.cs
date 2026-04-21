68adc1a911f5f67ccb785d538ccecc1554ab2ca3
Vlad119 <unstarkble12@gmail.com>
2026-04-21 14:34:44 +0300

    Решил задачу 14-1-5

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var students = new List<Student>
+    {
+        new Student("Anna", 23),
+        new Student("Max", 22),
+        new Student("Ivan", 24),
+        new Student("Oleg", 25)
+    };
+
+        var result = students.OrderBy(x => x.Age);
+
+        foreach (var student in result)
+        {
+            Console.WriteLine(student);
+        }
     }
 }
---
+++ MainApp/Student.cs
@@ -0,0 +1,14 @@
+﻿public class Student
+{
+    public string Name { get; }
+    public int Age { get; }
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public override string ToString()
+    {
+        return $"{Name}, {Age}";
+    }
+}
