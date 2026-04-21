432ac0405e36b6792fa5fb8dc6fb3aa8472d1011
Vlad119 <unstarkble12@gmail.com>
2026-04-21 12:09:53 +0300

    Решил задачу 14-1-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine()
+        .Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        var result = input.OrderBy(x => x).First();
+        Console.WriteLine(result);
     }
 }
