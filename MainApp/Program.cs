f90a15ec0195d98a7070f342e6369b257f8e6c86
Vlad119 <unstarkble12@gmail.com>
2026-04-22 10:21:26 +0300

    Решил задачу 14-1-7

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,14 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine()
+            .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+            .SelectMany(word => word.Where(char.IsLetter))
+            .Select(c => char.ToLower(c)).Distinct().OrderBy(x => x);
+        Console.WriteLine(string.Join(", ", input));
     }
 }
