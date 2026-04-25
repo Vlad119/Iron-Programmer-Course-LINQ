44bea642b3ef37727be46e443604022c644a3df9
Vlad119 <unstarkble12@gmail.com>
2026-04-25 14:09:22 +0300

    Решил задачу 14-1-9

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split()
+        .SelectMany(word => word.Where(char.IsLetter))
+        .OrderBy(x=>x).Last();
+        Console.WriteLine(input);
     }
 }
