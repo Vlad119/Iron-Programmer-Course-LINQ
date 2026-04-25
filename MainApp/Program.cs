e491dae3a6831c6f040b021ab424e55515977cea
Vlad119 <unstarkble12@gmail.com>
2026-04-25 13:55:40 +0300

    Решил задачу 14-1-8

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,23 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        int n = int.Parse(Console.ReadLine());
+        var entries = new List<(string name, TimeSpan time)>();
+        for (int i = 0; i < n; i++)
+        {
+            string line = Console.ReadLine();
+            int divide = line.LastIndexOf(',');
+            string name = line.Substring(0, divide).Trim();
+            string time = line.Substring(divide + 1).Trim();
+            TimeSpan ts = TimeSpan.Parse("0:" + time);
+            entries.Add((name, ts));
+        }
+        var lastPerson = entries.OrderBy(x => x.time).Last();
+        Console.WriteLine(lastPerson.name);
     }
 }
