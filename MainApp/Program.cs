0f542689853003fbff3523e295905e8b7ccf29ca
Vlad119 <unstarkble12@gmail.com>
2026-04-27 17:30:53 +0300

    Решил задачу 14-2-5

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        int number = int.Parse(Console.ReadLine());
+        int steps = 0;
+        while (number != 6174)
+        {
+            string numStr = number.ToString("D4");
+            var descendingDigits = numStr.Select(c => c).OrderByDescending(c => c);
+            string descending = new string(descendingDigits.ToArray());
+            var ascendingDigits = numStr.Select(c => c).OrderBy(c => c);
+            string ascending = new string(ascendingDigits.ToArray());
+            int min = int.Parse(ascending);
+            int max = int.Parse(descending);
+            number = max - min;
+            steps++;
+        }
+        Console.WriteLine(steps);
     }
 }
