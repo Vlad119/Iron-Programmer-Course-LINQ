c357dfbbc48cc3c4e611a21c29247bd43a23f919
Vlad119 <unstarkble12@gmail.com>
2026-04-27 10:13:04 +0300

    Решил задачу 14-2-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+internal class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        
+        var users = new List<User>
+    {
+        new User ("Иосиф", 27, 850),
+        new User ("Марк", 26, 1025),
+        new User ("Маша", 20, 12313)
+    };
+        var result = SortByFriendsCount(users);
+        foreach (var user in result)
+            Console.WriteLine(user.Name);
+    }
+
+    static IEnumerable<User> SortByFriendsCount(IEnumerable<User> users)
+    {
+        return users.OrderByDescending(x=>x.FriendsCount);
     }
 }
---
+++ MainApp/User.cs
@@ -0,0 +1,12 @@
+﻿public class User
+{
+    public string Name { get; }
+    public int Age { get; }
+    public int FriendsCount { get; }
+    public User(string name, int age, int friendsCount)
+    {
+        Name = name;
+        Age = age;
+        FriendsCount = friendsCount;
+    }
+}
