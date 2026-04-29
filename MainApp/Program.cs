53b36121792c9bc276645d79a83fb9ab495ed3ba
Vlad119 <unstarkble12@gmail.com>
2026-04-29 23:49:49 +0300

    Начал выполнять задачу 14-3-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -2,6 +2,32 @@
 {
     static void Main(string[] args)
     {
-        
+        var classroom1 = new ClassRoom(new List<Student>());
+        var classroom2 = new ClassRoom(new List<Student>());
+        ClassRoom[] classrooms = { classroom1, classroom2 };
+        //var orderedStudents = classrooms. . .
+    }
+}
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+
+    public Student(int age, string name)
+    {
+        Age = age;
+        Name = name;
+    }
+}
+
+public class ClassRoom
+{
+    public List<Student> Students { get; set; }
+
+    public ClassRoom(List<Student> students)
+    {
+        Students = students;
     }
 }
