a415f0e45dfe7ed7a86663ba4bdfc7e02f7ede00
Vlad119 <unstarkble12@gmail.com>
2026-05-03 19:15:20 +0300

    Решил задачу 14-3-7

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var points = new List<(int x, int y)>();
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            var coords = line.Split(' ');
+            int x = int.Parse(coords[0]);
+            int y = int.Parse(coords[1]);
+            points.Add((x, y));
+        }
+        var sortedPoints = points.OrderByDescending(x => x.x).ThenByDescending(y => y.y).ToList();
+        foreach (var point in sortedPoints)
+        {
+            Console.WriteLine($"{point.x} {point.y}");
+        }
     }
 }
