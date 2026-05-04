723c5357a050e650d85f8031bb7beb415e1ea13d
Vlad119 <unstarkble12@gmail.com>
2026-05-04 20:38:39 +0300

    Решил задачу 14-4-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var wagons = Console.ReadLine().Split(' ').ToList();
+        bool isFinalStation = bool.Parse(Console.ReadLine());
+        if (isFinalStation)
+        {
+            wagons.Reverse();
+        }
+        Console.WriteLine(string.Join(" ", wagons));
     }
 }
