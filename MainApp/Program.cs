f5036a30d128ffc922f0f840f5a9b37299f2b86a
Vlad119 <unstarkble12@gmail.com>
2026-05-04 15:20:11 +0300

    Решил задачу 14-4-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,16 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var palindromes = Enumerable.Range(1, 9)
+        .SelectMany(a => Enumerable.Range(0, 10),
+         (a, b) => new { a, b })
+        .SelectMany(x => Enumerable.Range(0, 10),
+         (x, c) => x.a * 10000 + x.b * 1000 + c * 100 + x.b * 10 + x.a);
+        int count = palindromes.Count();
+        Console.WriteLine(count);
     }
 }
