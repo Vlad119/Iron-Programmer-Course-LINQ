4228ab1ce925d64d8c34c2ce2d20bc805f014cde
Vlad119 <unstarkble12@gmail.com>
2026-05-27 21:13:29 +0300

    Решил задачу 15-1-3

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().ToLower().Split().SelectMany(x=>x)
+        .Distinct().OrderBy(x=>x).Where(char.IsLetterOrDigit);
+        var n = int.Parse(Console.ReadLine());
+        Console.WriteLine(string.Join("", input.Take(n)));
     }
 }
