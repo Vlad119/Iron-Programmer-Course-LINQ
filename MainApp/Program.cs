e51f5e27bc2c6b833f2f9f9c2d26266e8508c299
Vlad119 <unstarkble12@gmail.com>
2026-05-27 20:52:44 +0300

    Решил задачу 15-1-2

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
     static void Main(string[] args)
     {
-        
+        var input = Console.ReadLine().Split().Select(int.Parse);
+        var n = int.Parse(Console.ReadLine());
+        var sort = input.OrderByDescending(x => x);
+        Console.WriteLine(string.Join(" ", sort.Take(n)));
     }
 }
