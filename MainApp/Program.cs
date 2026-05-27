a63acbacd559703fdcea862792f7731b74046b81
Vlad119 <unstarkble12@gmail.com>
2026-05-27 21:39:01 +0300

    Решил задачу 15-1-4

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using System;
+using System.Linq;
+
+internal class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        
+        string input = Console.ReadLine();
+        int n = int.Parse(Console.ReadLine());
+        int result = input.Take(n).Where(char.IsDigit).Sum(c => c - '0');
+        Console.WriteLine(result);
     }
 }
